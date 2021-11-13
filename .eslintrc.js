bc3302cd5de117acd814adb3b4f8214b92d040f9
Gabriel Falcão (he/him/his) <gabriel.falcao@nytimes.com>
2021-11-13 10:34:56 +0100

    configure lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,42 @@
 module.exports = {
-    parser: "@babel/eslint-parser",
-    extends: ["eslint:recommended", "plugin:react/recommended"],
-    plugins: [
-        // ...
-        "react-hooks",
-        'prettier'
-    ],
-    rules: {
-        // ...
-        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-        "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
-        "no-unused-vars": "warn",
+  parser: "@babel/eslint-parser",
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  plugins: [
+    // ...
+    "react-hooks",
+    "prettier",
+  ],
+  rules: {
+    // ...
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+    "no-unused-vars": "warn",
+    "react/prop-types": "off",
+    "prettier/prettier": "error",
+  },
+  overrides: [
+    {
+      files: ["*.test.js", "**/__tests__/**/*.js"],
+      rules: {
+        "global-require": "off",
+        "no-console": "off",
+        "react/no-multi-comp": "off",
         "react/prop-types": "off",
-        "prettier/prettier": "error",
+        "no-await-in-loop": "off",
+        "no-restricted-syntax": "off",
+      },
     },
-    overrides: [
-        {
-            files: ["*.test.js", "**/__tests__/**/*.js"],
-            rules: {
-                "global-require": "off",
-                "no-console": "off",
-                "react/no-multi-comp": "off",
-                "react/prop-types": "off",
-                "no-await-in-loop": "off",
-                "no-restricted-syntax": "off",
-            },
-        },
-        {
-            files: ["src/main/**"],
-            rules: {
-                "no-undef": "off",
-            },
-        },
-    ],
-    env: {browser: true},
-    settings: {
-        react: {
-            version: "17.0.2", // React version. "detect" automatically picks the version you have installed.
-        },
+    {
+      files: ["src/main/**"],
+      rules: {
+        "no-undef": "off",
+      },
     },
+  ],
+  env: { browser: true },
+  settings: {
+    react: {
+      version: "17.0.2", // React version. "detect" automatically picks the version you have installed.
+    },
+  },
 };
